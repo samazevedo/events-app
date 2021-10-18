3e1f454b7677b1e1c0a24752d3cde7993581d641
Sam Azevedo <samuel.azevedo@live.com>
2021-10-18 16:35:07 -0300

    add  eventid.js page and additional components

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,7 +1,33 @@
+import { useRouter } from 'next/router'
+import { getEventById } from '../../event-data'
+import { Fragment } from 'react'
+import EventSummery from '../../components/event-detail/event-summary'
+import EventLogistics from '../../components/event-detail/event-logistics'
+import EventContent from '../../components/event-detail/event-content'
+
 function EventDetailPage() {
+    const router = useRouter()
+    const eventId = router.query.eventId
+    const event = getEventById(eventId)
+
+    if (!event) {
+        return <p>No event found!</p>
+    }
+
     return (
         <div>
-            <h1>Event Detail Page</h1>
+            <Fragment>
+                <EventSummery title={event.title} />
+                <EventLogistics
+                    date={event.date}
+                    address={event.location}
+                    image={event.image}
+                    imageAlt={event.title}
+                />
+                <EventContent>
+                    <p>{event.description}</p>
+                </EventContent>
+            </Fragment>
         </div>
     )
 }
