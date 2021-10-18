d4208218550c775619bdfecd69d854e2475793f7
Sam Azevedo <samuel.azevedo@live.com>
2021-10-18 06:30:53 -0300

    add styles & icons to components

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,4 +1,9 @@
-import Link from 'next/link'
+import styles from './event-item.module.css'
+import Button from '../ui/button'
+import DateIcon from '../icons/date-icon'
+import AddressIcon from '../icons/address-icon'
+import ArrowRighIcon from '../icons/arrow-right-icon'
+
 function EventItem(props) {
     const { title, image, date, location, id } = props
 
@@ -11,20 +16,28 @@ function EventItem(props) {
     const exploreLink = `/event/${id}`
 
     return (
-        <li>
+        <li className={styles.item}>
             <img src={'/' + image} alt={title} />
-            <div>
-                <div>
+            <div className={styles.content}>
+                <div className={styles.summary}>
                     <h2>{title}</h2>
+                    <div className={styles.date}>
+                        <DateIcon />
+
+                        <time>{humanReadableDate}</time>
+                    </div>
+                    <div className={styles.address}>
+                        <AddressIcon />
+                        <address>{formattedAddress}</address>
+                    </div>
                 </div>
-                <div>
-                    <time>{humanReadableDate}</time>
-                </div>
-                <div>
-                    <address>{formattedAddress}</address>
-                </div>
-                <div>
-                    <Link href={exploreLink}>Explore Events</Link>
+                <div className={styles.actions}>
+                    <Button link={exploreLink}>
+                        <span>Explore Event</span>
+                        <span className={styles.icon}>
+                            <ArrowRighIcon />
+                        </span>
+                    </Button>
                 </div>
             </div>
         </li>
--- components/events/event-list.js
+++ components/events/event-list.js
@@ -1,8 +1,10 @@
 import EventItem from './event-item'
+import styles from './event-list.module.css'
+
 function EventList(props) {
     const { events } = props
     return (
-        <ul>
+        <ul className={styles.list}>
             {events.map((event) => (
                 <EventItem
                     key={event.id}
